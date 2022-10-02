723bda56436c402c5eb4c5a2eb11c04edb141f44
Keijur0 <tran.qlam@gmail.com>
2022-10-02 18:49:22 +0200

    Water test done

---
+++ eau15.js
@@ -0,0 +1,3 @@
+// This script celebrates the end of the Water test! :)
+
+console.log("J'ai terminé l'épreuve de l'Eau et c'était plus simple que je ne le pensais. L'epreuve de la Terre m'a bien entrainé et les conseils de Harry également. Merci Harry ;)")
